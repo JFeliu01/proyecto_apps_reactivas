349ce2bfc3aa47240fe43f0e56e953917074a2c6
Jorge Feliu <jorgefeliu.ag@gmail.com>
2025-09-21 17:20:15 -0300

    initial commit: proper format of the proyect

---
+++ lolgg/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ChampionsView from './components/ChampionsView'
+
+function App() {
+  return (
+    <>
+      <h1 style={{ marginBottom: 16 }}>Champions</h1>
+      <ChampionsView />
+    </>
+  )
+}
+
+export default App
